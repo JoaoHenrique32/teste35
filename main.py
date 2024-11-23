ca28ce0fe58d0ebe789c5a68ecebb3c4ec593217
JoaoHenrique32 <janjan.henrique40@gmail.com>
2024-11-23 16:39:06 -0300

    testando

---
+++ main.py
@@ -0,0 +1,27 @@
+from json_db import JsonDB
+from product import Product
+
+from fastapi import FastAPI
+
+from indb import generate_products
+
+
+
+app = FastAPI()
+
+productDB = JsonDB(path='./data/products.json')
+
+
+@app.get('/products')
+def get_products():
+    products = productDB.read()
+    return { "products" : products }
+
+
+@app.post('/products')
+def create_product(product: Product):
+
+    productDB.insert(product)
+
+    return {"status": "inserted"}
+
